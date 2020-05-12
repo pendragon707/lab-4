3273f79b770107c64f50805ad0e19a1ec4d1ac25
Nonpenguin <pendragon707@mail.ru>
2020-05-12 19:35:21 +0300

    Add threads

--- 4/main.cpp
+++ 4/main.cpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <windows.h>
 #include <tchar.h>
 #include <gdiplus.h>
@@ -6,9 +8,16 @@
 using namespace Gdiplus;
 
 static LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+DWORD WINAPI circle_thread_proc(void*);
+DWORD WINAPI text_thread_proc(void*);
 
 static const TCHAR classname[] = TEXT("Lab4");
 static const TCHAR title[] = TEXT("Lab4");
+
+int window_wid, window_hei;
+
+HANDLE circle_thread, circle_start_event;
+HANDLE text_thread, text_start_event, text_end_event;
 
 HBRUSH bg_brush = 0;
 int WINAPI _tWinMain(HINSTANCE hInst, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nShowCmd)
@@ -73,6 +82,23 @@ static LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM l
 {
   switch (message)
   {
+	  case WM_CREATE:
+    {
+      circle_start_event = CreateEvent(0, FALSE, FALSE, TEXT("CircleStartEvent"));
+      circle_thread = CreateThread(0, 0, circle_thread_proc, 0, 0, 0);
+
+      text_start_event = CreateEvent(0, FALSE, FALSE, TEXT("TextStartEvent"));
+      text_end_event = CreateEvent(0, FALSE, FALSE, TEXT("TextEndEvent"));
+      text_thread = CreateThread(0, 0, text_thread_proc, 0, 0, 0);
+
+      SetTimer(hWnd, 0, 33, 0);
+      break;
+    }
+	 case WM_TIMER:
+    {
+      InvalidateRect(hWnd, 0, FALSE);
+      break;
+    }
     case WM_DESTROY:
     {
       PostQuitMessage(0);
@@ -81,3 +107,24 @@ static LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM l
   }
   return DefWindowProc(hWnd, message, wParam, lParam);
 }
+
+DWORD WINAPI circle_thread_proc(void*)
+{
+  for (;;)
+  {
+    WaitForSingleObject(circle_start_event, INFINITE);
+
+    SetEvent(text_start_event);
+  }
+}
+
+DWORD WINAPI text_thread_proc(void*)
+{
+  for (;;)
+  {
+    WaitForSingleObject(text_start_event, INFINITE);
+
+    SetEvent(text_end_event);
+  }
+}
+
